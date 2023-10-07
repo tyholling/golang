ea0c72402fc2add5ae4508af88e03c330ab593a2
Tyler Hollingsworth <tyholling@gmail.com>
2023-10-07 08:17:41 -0700

    added http

---
+++ cmd/http/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "time"
+)
+
+func main() {
+  http.HandleFunc("/", handle)
+
+  server := http.Server{
+    ReadTimeout: time.Minute,
+  }
+  err := server.ListenAndServe()
+  if err != nil {
+    log.Fatalln(err)
+  }
+}
+
+func handle(w http.ResponseWriter, _ *http.Request) {
+  w.WriteHeader(http.StatusOK)
+
+  data := []byte("\n")
+  _, err := w.Write(data)
+  if err != nil {
+    log.Fatalln(err)
+  }
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tyholling/golang
+
+go 1.21.2
