9124020b1ce1c592928d4933ffa74ebaa53fadc9
Tyler Hollingsworth <tyholling@gmail.com>
2023-10-07 15:43:10 -0700

    added grpc server impl

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
-build: tidy
+build: buf tidy
 	go build -o bin/ ./...
+
+buf:
+	buf lint
+	buf generate
 
 format:
 	gofumpt -w -extra .
@@ -16,6 +20,9 @@ vuln:
 check: format lint
 
 setup:
+	go install github.com/bufbuild/buf/cmd/buf@latest
 	go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 	go install golang.org/x/vuln/cmd/govulncheck@latest
+	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+	go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 	go install mvdan.cc/gofumpt@latest
--- cmd/grpc/server/main.go
+++ cmd/grpc/server/main.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 
+	pb "github.com/tyholling/golang/proto/grpc/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
 )
 
 type Server struct {
@@ -18,9 +22,10 @@ func (s *Server) ListenAndServe() error {
 	if err != nil {
 		return fmt.Errorf("failed to listen: %w", err)
 	}
-
 	s.listener = listener
+
 	s.server = grpc.NewServer()
+	pb.RegisterConnectionServiceServer(s.server, &connectionServer{})
 
 	err = s.server.Serve(s.listener)
 	if err != nil {
@@ -28,6 +33,65 @@ func (s *Server) ListenAndServe() error {
 	}
 
 	return nil
+}
+
+type connectionServer struct {
+	pb.UnimplementedConnectionServiceServer
+}
+
+func (s *connectionServer) Connect(stream pb.ConnectionService_ConnectServer) error {
+	wg := sync.WaitGroup{}
+	messages := make(chan *pb.ConnectResponse)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		handleSend(stream, messages)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		handleRecv(stream, messages)
+	}()
+
+	wg.Wait()
+	return nil
+}
+
+func handleSend(stream pb.ConnectionService_ConnectServer, messages <-chan *pb.ConnectResponse) {
+	for msg := range messages {
+		err := stream.Send(msg)
+		if err != nil {
+			log.Fatalf("failed to send: %s", err)
+		}
+		log.Printf("send: %s", msg)
+	}
+}
+
+func handleRecv(stream pb.ConnectionService_ConnectServer, _ chan<- *pb.ConnectResponse) {
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("failed to receive: %s", err)
+		}
+
+		if msg.Request != nil {
+			request, err := anypb.UnmarshalNew(msg.Request, proto.UnmarshalOptions{})
+			if err != nil {
+				log.Print(err)
+				continue
+			}
+			log.Printf("received request: %s", request)
+		} else if msg.Response != nil {
+			response, err := anypb.UnmarshalNew(msg.Response, proto.UnmarshalOptions{})
+			if err != nil {
+				log.Print(err)
+				continue
+			}
+			log.Printf("received response: %s", response)
+		}
+	}
 }
 
 func main() {
