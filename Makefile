b370efd8bad902986076b4d6538b478a4005e9f6
Tyler Hollingsworth <tyholling@gmail.com>
2023-10-07 08:22:40 -0700

    added make format

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 build:
 	go build -o bin/ ./...
+
+format:
+	gofumpt -w -extra .
+
+setup:
+	go install mvdan.cc/gofumpt@latest
--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "time"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-  http.HandleFunc("/", handle)
+	http.HandleFunc("/", handle)
 
-  server := http.Server{
-    ReadTimeout: time.Minute,
-  }
-  err := server.ListenAndServe()
-  if err != nil {
-    log.Fatalln(err)
-  }
+	server := http.Server{
+		ReadTimeout: time.Minute,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func handle(w http.ResponseWriter, _ *http.Request) {
-  w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 
-  data := []byte("\n")
-  _, err := w.Write(data)
-  if err != nil {
-    log.Fatalln(err)
-  }
+	data := []byte("\n")
+	_, err := w.Write(data)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
